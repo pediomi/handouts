508eb0bfd4e82d1231fe58ba4ae9a0bc83fad2a4
Ediomi David <pediomi@yahoo.com>
2023-01-01 19:49:36 +0000

    My first commit

---
+++ worksheet.sh
@@ -0,0 +1,13 @@
+# Configure git
+
+# This line may be needed to change the default branch name from master to main.
+git config --global init.defaultBranch main 
+
+# Replace the ... in these lines with the user name and email address
+# you have already set up on GitHub.com.
+git config --global user.name ...
+git config --global user.email ...
+
+# Link your local repository to the origin repository on GitHub, by
+# copying the code shown on your GitHub repo under the heading:
+# "…or push an existing repository from the command line"
